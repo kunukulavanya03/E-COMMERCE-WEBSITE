65784e6ed72a1eccd61248c7de20efd5a04310d7
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-12-15 15:36:29 +0530

    🚀 Initial commit - Generated with CODE AGENT

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import '../pages/home.css';
+import Hero from '../components/Hero'; 
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Hero />
+
+    </div>
+  );
+};
+
+export default Home;
