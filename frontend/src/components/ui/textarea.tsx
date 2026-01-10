f0fb2582fac21428bb4b23eaf8311d12baa7db73
kunukulavanya03 <kunukulavanya03@gmail.com>
2026-01-10 10:33:51 +0530

    🚀 Initial commit - Generated with CODE AGENT

--- frontend/src/components/ui/textarea.tsx
+++ frontend/src/components/ui/textarea.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import { cn } from "./utils";
-import { logout, createAuthregister, createAuthlogin, getProducts{product_id}, getCategories, getProducts, updateCartupdate, deleteCartremove{product_id}, getOrders, login } from './services/api';
+import { createAuthlogin, updateProducts{product_id}, deleteProducts{product_id}, createAuthregister, getProducts{product_id}, createCartadd{product_id}, getOrders{order_id}, getCart, createProducts, getCategories } from './services/api';
 
 function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
   return (
